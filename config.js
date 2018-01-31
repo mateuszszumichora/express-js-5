415b9e3e724bf1a6f371d8a5cb1f76d343211536
Mateusz Szumichora <mateuszszumichora@gmail.com>
2018-01-31 19:49:10 +0100

    Now fine

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    'GOOGLE_CLIENT_ID' : '723478780994-qkh2cvsg7vdrarq8dsbikr5u6visdeup.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET' : '8secEWZZRFgtN3nxqe31_po6',
+    'GOOGLE_CLIENT_ID' : '723478780994-2d1tv426jo22lq08v22rdsk7up40sq20.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET' : 'TnFUsl4Q3rWMCIpVFztEW8Ts',
     'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
 };
+
