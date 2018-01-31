027a1b3ec1a41fa64d237a8d93ef7b28897997e9
Mateusz Szumichora <mateuszszumichora@gmail.com>
2018-01-31 19:17:17 +0100

    Error

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID' : '723478780994-qkh2cvsg7vdrarq8dsbikr5u6visdeup.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET' : '8secEWZZRFgtN3nxqe31_po6',
+    'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
